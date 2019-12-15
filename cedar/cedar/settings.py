4118e59344d4f843a6f6219bddd1371b9e0d9ae2
Emimake <emysteven@gmail.com>
2019-12-15 20:13:44 +0100

    Postgress DB and static root changes

--- cedar/cedar/settings.py
+++ cedar/cedar/settings.py
@@ -86,13 +86,12 @@ WSGI_APPLICATION = 'cedar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER' : 'root',
-        'PASSWORD':'',
-        'HOST' : '',
-        'PORT' : '',
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': 'blog_admin',
+        'PASSWORD': 'password@231',
+        'HOST': 'localhost',
+        'PORT': '',
       
     }
 }
